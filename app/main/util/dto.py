7beebfb959c8296a8dcacba0ade5ff7eaa8ccf15
tungphan311 <thanhtunga1lqd@gmail.com>
2020-10-16 19:03:45 +0700

    user controller + set up swagger

---
+++ app/main/util/dto.py
@@ -0,0 +1,12 @@
+from flask_restx import Namespace, fields
+
+
+class UserDto:
+    api = Namespace(
+        'User', description='user related operations')
+    user = api.model('user', {
+        'email': fields.String(required=True, description='user email address'),
+        'username': fields.String(required=True, description='user username'),
+        'password': fields.String(required=True, description='user password'),
+        # 'public_id': fields.String(description='user Identifier')
+    })
--- manage.py
+++ manage.py
@@ -4,10 +4,12 @@ import unittest
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
+from app import blueprint
 from app.main import create_app, db
 from app.main.model import user
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+app.register_blueprint(blueprint)
 
 app.app_context().push()
 
@@ -17,9 +19,11 @@ migrate = Migrate(app, db)
 
 manager.add_command('db', MigrateCommand)
 
+
 @manager.command
 def run():
     app.run()
+
 
 @manager.command
 def test():
@@ -29,6 +33,7 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
 
 if __name__ == '__main__':
     manager.run()
