40dd0124cd4d5a964f8d31532cdc340977791a7c
tungphan311 <thanhtunga1lqd@gmail.com>
2020-11-13 18:06:53 +0700

    add environtment variable file

--- app/main/config.py
+++ app/main/config.py
@@ -15,14 +15,14 @@ class DevelopmentConfig(Config):
     # uncomment the line below to use postgres
     # SQLALCHEMY_DATABASE_URI = postgres_local_base
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://thesis:tung3101@localhost/resume-screening'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://thesis:tung3101@localhost/resume-screening'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     PRESERVE_CONTEXT_ON_EXCEPTION = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
