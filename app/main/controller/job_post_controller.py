3d90a878038fc4697154ca6884478f1290dd174e
tungphan311 <thanhtunga1lqd@gmail.com>
2020-12-20 05:31:44 +0700

    update model + get job list api

--- app/main/controller/job_post_controller.py
+++ app/main/controller/job_post_controller.py
@@ -1,10 +1,11 @@
-from flask_jwt_extended.utils import get_jwt_identity
 from app.main.util.custom_jwt import HR_only
-from flask import app, request
+from flask import request
 
 from ..dto.job_post_dto import JobPostDto
 from flask_restx import Resource
 from ..service.job_post_service import add_new_post, candidate_get_job_posts, get_hr_posts
+
+from app.main.config import Config as config
 
 api = JobPostDto.api
 _job_post = JobPostDto.job_post
@@ -23,10 +24,17 @@ class JobPost(Resource):
     def get(self):
         is_hr = request.args.get('is_hr') == 'true'
 
-        page = request.args.get('page', app.config['DEFAULT_PAGE'], type=int)
-        page_size = request.args.get('page-size', app.config['DEFAULT_PAGE_SIZE'], type=int)
+        page = request.args.get('page', config.DEFAULT_PAGE, type=int)
+        page_size = request.args.get('page-size', config.DEFAULT_PAGE_SIZE, type=int)
+        posted_in = request.args.get('posted_in', 0, type=int)
+        deadline = request.args.get('deadline', 0, type=int)
+        view = request.args.get('view', 0, type=int)
+        apply = request.args.get('apply', 0, type=int)
+        save = request.args.get('save', 0, type=int)
+
+        sort_values = { 'posted_in': posted_in, 'deadline': deadline, 'view': view, 'apply': apply, 'save': save }
 
         if is_hr:
-            return get_hr_posts(page, page_size)
+            return get_hr_posts(page, page_size, sort_values)
         else:
             return candidate_get_job_posts()
