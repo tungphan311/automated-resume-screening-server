cb3481a57239ecc48eb6923d4ab84f0d6c8971b5
tungphan311 <thanhtunga1lqd@gmail.com>
2021-01-19 22:47:18 +0700

    update resume api

--- app/main/service/resume_service.py
+++ app/main/service/resume_service.py
@@ -61,7 +61,7 @@ def update_cv(args):
     resume.resume_id = args['resume_id']
     resume.educations = args['educations']
     resume.experiences = args['experiences']
-    resume.skills = args['skills']
+    resume.technical_skills = args['skills']
     resume.months_of_experience = args['months_of_experience']
 
     domain_id = args['job_domain_id']
@@ -69,7 +69,10 @@ def update_cv(args):
         abort(400)
 
     resume.job_domain_id = domain_id
+
+    db.session.add(resume)
     db.session.commit()
+
     return resume
     
 def delete_cv_by_cand_id(id):
